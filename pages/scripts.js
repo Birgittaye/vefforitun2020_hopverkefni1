205df7dff8be561bedf0e91c75722b4fa0537380
Birgitta Ýr <birgittaye@gmail.com>
2020-11-02 22:55:36 +0000

    nav

---
+++ pages/scripts.js
@@ -0,0 +1,8 @@
+function changeNav() {
+  let x = document.getElementById("nav");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
